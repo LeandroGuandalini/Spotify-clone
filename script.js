4ec50fe393971987b00a0321e71fc2d4a4449c7a
Leandro Guandalini <leandro.gribeiro@al.infnet.edu.br>
2024-10-10 19:42:46 -0300

    progressão do css e adição de resize no js

---
+++ script.js
@@ -0,0 +1,37 @@
+const dividerNav = document.querySelector('.divider-nav');
+const dividerInfo = document.querySelector('.divider-info');
+const nav = document.querySelector('.nav')
+const infoPlayling = document.querySelector('.info-playing');
+
+let isResizing = false;
+let activeDivider = null;
+
+dividerNav.addEventListener('mousedown', (event) => {
+  isResizing = true;
+  activeDivider = 'nav';
+  document.body.style.userSelect = 'none';
+});
+
+dividerInfo.addEventListener('mousedown', (event) => {
+  isResizing = true;
+  activeDivider = 'infoPlaying';
+  document.body.style.userSelect = 'none';
+});
+
+document.addEventListener('mousemove', (event) => {
+  if (!isResizing) return;
+
+  if (activeDivider === 'nav') {
+    const newWidth = event.clientX - nav.getBoundingClientRect().left;
+    nav.style.width = `${newWidth}px`;
+  } else if (activeDivider === 'infoPlaying') {
+    const newWidth = event.clientX - infoPlaying.getBoundingClientRect().left;
+    infoPlaying.style.width = `${newWidth}px`;
+  }
+});
+
+document.addEventListener('mouseup', () => {
+  isResizing = false;
+  activeDivider = null; // Reseta o 'divider' ativo
+  document.body.style.userSelect = 'auto';
+});
