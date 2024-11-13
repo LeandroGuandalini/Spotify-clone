53184053fa68623796af0bd5b5f11b5208eff2ee
Leandro Guandalini <leandro.gribeiro@al.infnet.edu.br>
2024-11-13 14:54:10 -0300

    progress in style

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const dividerNav = document.querySelector('.divider-nav');
 const dividerInfo = document.querySelector('.divider-info');
-const nav = document.querySelector('.nav')
+const nav = document.querySelector('.nav');
 const infoPlaying = document.querySelector('.info-playing');
+const center = document.querySelector('.center'); // Certifique-se de que a div center seja selecionada corretamente.
 
 let isResizing = false;
 let activeDivider = null;
@@ -24,14 +25,42 @@ document.addEventListener('mousemove', (event) => {
   if (activeDivider === 'nav') {
     const newWidth = event.clientX - nav.getBoundingClientRect().left;
     nav.style.width = `${newWidth}px`;
+
+    const newWidthCenter = window.innerWidth - newWidth - infoPlaying.offsetWidth;
+    center.style.width = `${newWidthCenter}px`;
   } else if (activeDivider === 'infoPlaying') {
-    const newWidth = event.clientX - infoPlaying.getBoundingClientRect().left;
+    const newWidth =  infoPlaying.getBoundingClientRect().right - event.clientX;
     infoPlaying.style.width = `${newWidth}px`;
+
+    const newWidthCenter = window.innerWidth - newWidth - nav.offsetWidth;
+    center.style.width = `${newWidthCenter}px`;
   }
 });
 
 document.addEventListener('mouseup', () => {
   isResizing = false;
-  activeDivider = null; 
+  activeDivider = null;
   document.body.style.userSelect = 'auto';
 });
+
+const navContent = document.querySelector('.nav-content')
+
+// Função para mostrar a barra de rolagem
+function showScrollbar(event) {
+  event.target.style.overflow = 'auto';  // Mostra a barra de rolagem
+}
+
+// Função para esconder a barra de rolagem
+function hideScrollbar(event) {
+  event.target.style.overflow = 'hidden';  // Esconde a barra de rolagem
+}
+
+// Adicionando ouvintes de evento para as divs
+navContent.addEventListener('mouseenter', showScrollbar);
+center.addEventListener('mouseenter', showScrollbar);
+infoPlaying.addEventListener('mouseenter', showScrollbar);
+
+// Remover a barra de rolagem quando o mouse sair
+navContent.addEventListener('mouseleave', hideScrollbar);
+center.addEventListener('mouseleave', hideScrollbar);
+infoPlaying.addEventListener('mouseleave', hideScrollbar);
